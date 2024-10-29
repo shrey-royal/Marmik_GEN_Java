91b94fb83d1c5a3786adefda8a927a7fe9b86680
Shrey K <srkkadia@gmail.com>
2024-10-29 15:14:10 +0530

    Intro to Java

---
+++ Basics/Basic.java
@@ -0,0 +1,9 @@
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // for (int i = 0; i < args.length; i++) {
+        //     System.out.println(args[i]);
+        // }
+    }
+}
